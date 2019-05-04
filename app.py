3d47e45371fdc74c84c36762291a68c03d4ba79b
Olivia M <olivia.martin.473@gmail.com>
2019-05-03 20:19:33 -0500

    app.py working, this version can load in pollution data only, will work on adding other tables to sqlite file to load in all_data.csv and cancer.csv

--- app.py
+++ app.py
@@ -14,13 +14,12 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, Column, Integer, Float, String
 
 from flask import Flask, jsonify, render_template
-# from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/cancer_pollution_db.sqlite"
 db = SQLAlchemy(app)
 
-class Pollution(db.Model):
+class pollution(db.Model):
     __tablename__ = 'pollution_table'
 
     id = Column(Integer, primary_key=True)
@@ -34,12 +33,7 @@ class Pollution(db.Model):
     SO2 = Column(Float)
     State = Column(String)
     Year = Column(Integer)
-
-    def __init__(id, self, CO, County_Code, Lead, NO2, Ozone, PM10, PM2_5,
-                SO2, State, Year):
-                self. id = id
-                
-
+    
     def __repr__(self):
         return '<Pollutant %r>' % (self.CO)
 
@@ -53,23 +47,46 @@ def setup():
 # create route that renders index.html template
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", pollution=pollution)
 
 
 # Query the database and send the jsonified results
 @app.route("/test")
 def list_pollutants():
-    return jsonify(id=Column(Integer, primary_key=True)
-    CO = Column(Float)
-    County_Code = Column(Float)
-    Lead = Column(Float)
-    NO2 = Column(Float)
-    Ozone = Column(Float)
-    PM10 = Column(Float)
-    PM2_5 = Column(Float)
-    SO2 = Column(Float)
-    State = Column(String)
-    Year = Column(Integer))
+    results = db.session.query(
+        pollution.CO,
+        pollution.County_Code,
+        pollution.Lead,
+        pollution.NO2,
+        pollution.Ozone,
+        pollution.PM10,
+        pollution.PM2_5,
+        pollution.SO2,
+        pollution.State,
+        pollution.Year
+    )
+
+    data = results.all()
+
+    pollution_data = []
+    for d in data[:15]:
+
+        print('data:', d)
+
+        pollution_data.append({
+            "CO": d[0],
+            "County_Code": d[1],
+            "Lead": d[2],
+            "NO2": d[3],
+            "Ozone": d[4],
+            "PM10": d[5],
+            "PM2_5": d[6],
+            "SO2": d[7],
+            "State": d[8],
+            "Year": d[9]
+        })
+
+    return json.dumps(pollution_data)
 
 
 if __name__ == "__main__":
