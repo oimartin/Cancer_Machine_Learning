922ba46879fd72969a897ff8f8f26e9772f69762
smurphy8 <stephen.murphy1@northwestern.edu>
2019-05-08 21:56:51 -0500

    GGGGG

--- Painful_Project/Jupyter Notebook/test.py
+++ Painful_Project/Jupyter Notebook/test.py
@@ -2,3 +2,6 @@ from functions import getNNvalues, NNpredict
 
 values = getNNvalues("IL", "White", 2000.0, "Male")
 print(NNpredict(values))
+
+values = getNNvalues("CA", "White", 2015.0, "Male")
+print(NNpredict(values))
