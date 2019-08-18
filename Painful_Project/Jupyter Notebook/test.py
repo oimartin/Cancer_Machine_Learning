3cb4745da9ad83c4e8383d735de682ae412e4d2d
Olivia M <olivia.martin.473@gmail.com>
2019-08-18 18:39:40 -0500

    updating files according to last repo created

--- Painful_Project/Jupyter Notebook/test.py
+++
@@ -1,7 +0,0 @@
-from functions import getNNvalues, NNpredict
-
-values = getNNvalues("IL", "White", 2000.0, "Male")
-print(NNpredict(values))
-
-values = getNNvalues("CA", "White", 2015.0, "Male")
-print(NNpredict(values))
